b07edef46c3d51b31144d14390a4f357874d6ff1
tngs <s.tengges2@gmail.com>
2025-05-22 23:19:41 +0900

    transporter specific home page

--- src/pages/Transporter/Home/index.js
+++ src/pages/Transporter/Home/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./index.css";
 import ServiceList from "../../../components/List/ServiceList";
 import services from "../../../servicesDB.json";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const user = useSelector((state) => state.account.user);
+  console.log('user', user)
+  console.log('services', services)
+  const filteredServices = services.filter(service => service.transporterId === user.id);
+  console.log('filteredServices', filteredServices)
   return (
     <div className="container-centers-top">
-      <ServiceList services={services} />
+      <ServiceList services={filteredServices} />
     </div>
   );
 };
