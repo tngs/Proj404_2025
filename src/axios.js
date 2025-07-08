9050e727648853f8ca5ed1c836ac1a5650d732c1
tngs <s.tengges2@gmail.com>
2025-07-08 16:47:23 +0900

    changed url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'http://localhost:3050'
-    baseURL: 'http://192.168.0.118:8080',
+    baseURL: 'http://34.229.134.122:8080',
     withCredentials: true,
 });
 
